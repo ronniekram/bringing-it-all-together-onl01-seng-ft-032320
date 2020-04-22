7594acf720132dbb214c9cf8007dd13215dde1bb
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-22 00:04:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,6 +34,8 @@ class Dog
 
         DB[:conn].execute(sql, self.name, self.breed)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0] 
+        
+        self
     end 
 
     def self.create(name:, breed:)
