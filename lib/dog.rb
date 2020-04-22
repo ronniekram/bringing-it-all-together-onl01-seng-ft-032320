b1af98830185aa6607e3270b8b4e0752343da8f1
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-22 01:29:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,9 +43,9 @@ class Dog
 
   def self.new_from_db(row)
     dog_hash = {
-        :id => row[0]
-        :name => row[1]
-        :breed => row[2]
+      :id => row[0]
+      :name => row[1]
+      :breed => row[2]
     }
     
     self.new(dog_hash)
