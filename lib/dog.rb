06afd2092db3df6cd0ba61d0ebaf32dda6119eeb
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-22 00:32:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,24 @@
 class Dog
-    attr_accessor :id, :name, :breed
+  attr_accessor :id, :name, :breed
 
-    @@all = []
+  @@all = []
 
-    def initialize(attributes)
-        attributes.each {|key, value| self.send(("#{key}="), value)}
-        self.id ||= nil
-    end
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
 
-    def self.create_table
-        sql =<<-SQL
-        CREATE TABLE IF NOT EXISTS dogs(
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        breed TEXT
-        )
-        SQL
+  def self.create_table
+    sql =<<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
+      SQL
         
-        DB[:conn].execute(sql)
-    end 
+      DB[:conn].execute(sql)
+  end 
 
     def self.drop_table
         DB[:conn].execute("DROP TABLE IF EXISTS dogs") 
@@ -32,20 +32,15 @@ class Dog
 
         DB[:conn].execute(sql, self.name, self.breed)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0] 
-        
-        self
     end 
 
-    def self.create(name, breed)
-        dog = self.new(name, breed)
-        dog.name = name 
-        dog.breed = breed
-        dog.save 
-        dog
+    def self.create(dogs_hash)
+        dog = self.new(dog_hash)
+        dog.save
     end 
 
     def self.new_from_db(row)
-        new_dog = self.new 
+        new_dog = Dog.new 
         new_dog.id = row[0]
         new_dog.name = row[1]
         new_dog.breed = row[2]
