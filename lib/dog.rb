8087953a0e6b3b5a49b7c5b3ce78ba4a3864851f
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-22 01:06:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,6 +62,8 @@ class Dog
         if !dog.empty?
             dog_info = dog[0]
             dog = Dog.new(dog_info[0], dog_info[1], dog_info[2])
+        elsif !(dog.name && dog.breed)
+        
         else 
             dog = Dog.create(name: name, breed: breed)
         end 
