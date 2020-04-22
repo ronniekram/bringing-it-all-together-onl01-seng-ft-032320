cb4557e5325a245593efcaa5b9726edaa6ff69b7
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-22 01:08:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,7 +57,9 @@ class Dog
     end 
 
     def self.find_or_create_by(name:, breed:)
-      dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed) 
+      sql = <<-SQL 
+      
+      SQL
       dog 
     end 
 
