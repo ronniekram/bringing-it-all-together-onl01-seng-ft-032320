3680ec0a722ac3a20757a079d4ab3b4e654b25b4
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-22 00:35:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,11 +40,11 @@ class Dog
     end 
 
     def self.new_from_db(row)
-        new_dog = Dog.new 
-        new_dog.id = row[0]
-        new_dog.name = row[1]
-        new_dog.breed = row[2]
-        new_dog 
+        dog = Dog.new 
+        dog.id = row[0]
+        dog.name = row[1]
+        dog.breed = row[2]
+        dog 
     end 
 
     def self.find_by_id(id)
