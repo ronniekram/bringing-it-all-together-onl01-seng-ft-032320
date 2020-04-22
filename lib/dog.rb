3260fdd8bb73e7cc73a41b2c39748d273015fbe3
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-22 01:16:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,33 +37,39 @@ class Dog
     end 
 
     def self.create(dog_hash)
-        dog = Dog.new(dog_hash)
-        dog.save
+      dog = Dog.new(dog_hash)
+      dog.save
     end 
 
     def self.new_from_db(row)
-        dog = Dog.new(id, name, breed) 
-        dog.id = row[0]
-        dog.name = row[1]
-        dog.breed = row[2]
-        dog 
-    end 
-
-    def self.find_by_id(id)
-        sql = "SELECT * FROM dogs WHERE id = ?"
-        result = DB[:conn].execute(sql, id)[0]
-        Dog.new(result[0], result[1], result[2])
-
-    end 
-
-    def self.find_or_create_by(name:, breed:)
-      sql = <<-SQL 
-        SELECT * FROM dogs WHERE name = ? AND breed = ?
-      SQL
-      dog = DB[:conn].execute(sql, name, breed).first 
-      
+      dog = Dog.new(id, name, breed) 
+      dog.id = row[0]
+      dog.name = row[1]
+      dog.breed = row[2]
       dog 
     end 
+
+  def self.find_by_id(id)
+      sql = "SELECT * FROM dogs WHERE id = ?"
+      result = DB[:conn].execute(sql, id)[0]
+      Dog.new(result[0], result[1], result[2])
+
+  end 
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL 
+      SELECT * FROM dogs WHERE name = ? AND breed = ?
+    SQL
+    dog = DB[:conn].execute(sql, name, breed).first
+
+    if dog 
+      new_dog = self.new_from_db(dog)
+    else 
+      new_dog = self.create({:name => name, :breed => breed})
+    end
+
+    dog 
+  end  
 
   def self.find_by_name(name)
     sql = <<-SQL
